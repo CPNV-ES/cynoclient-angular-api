20d1a87db59ea8ef4a829cac95c3c66dbaeadbe9
dlmw <12473240+dlmw@users.noreply.github.com>
2020-11-26 13:56:35 +0100

    Create initial Client model

---
+++ src/models/client.model.ts
@@ -0,0 +1,24 @@
+import {Table, Column, Model} from 'sequelize-typescript';
+
+@Table
+export class Client extends Model<Client> {
+    @Column
+    firstname: string;
+
+    @Column
+    lastname: string;
+
+    @Column
+    female: number;
+
+    @Column
+    email: string;
+
+    @Column
+    phone: string;
+
+    @Column
+    street: string;
+
+
+}
