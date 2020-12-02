949626a2869306595dd9564989313496cdbff3bd
Killian Viquerat <kil.viquerat@gmail.com>
2020-12-02 11:37:52 +0100

    Implement post,put and delete method for controller Client

--- src/controllers/client.controller.ts
+++ src/controllers/client.controller.ts
@@ -1,28 +1,53 @@
 import {Request, Response} from "express";
+import { ClientRequest } from "http";
+import { where } from "sequelize/types";
 import {Client} from "../models/client.model";
 
 // POST
 export async function post(req: Request, res: Response) {
-    console.log(req.body.firstname);
-    res.status(200).send("DONE " + req.body.firstname);
+    try{
+        const client = await Client.create(req.body)
+        res.status(201).send("DONE");
+    } catch (error){
+        res.status(500).send("Internal Server Error")
+    }
 }
 // GET BY ID
 export async function getById(req: Request, res: Response) {
-    const client = await Client.findByPk(req.params.id);
-    res.status(200).send(client);
+    try{
+        const client = await Client.findByPk(req.params.id);
+        res.status(200).send(client);
+    } catch (error){
+        res.status(404).send("Client not found")
+    }
 }
 // GET
 export async function get(req: Request, res: Response) {
-    const clients = await Client.findAll();
-    res.status(200).send(clients);
+    try{
+        const clients = await Client.findAll();
+        res.status(200).send(clients);
+    } catch (error){
+        res.status(500).send("Internal Server Error")
+    }
 }
 // PUT
 export async function put(req: Request, res: Response) {
-    res.status(200).send("DONE " + req.body.firstname);
+    try{
+        const client = await Client.update(req.body,{where:{id:req.params.id}})
+        res.status(200).send("DONE");
+    } catch (error){
+        res.status(500).send("Internal Server Error");
+    }
 }
 // REMOVE
 export async function remove(req: Request, res: Response) {
-    res.status(200).send("DONE " + req.params.id);
+    try{
+        const client = await Client.destroy({where:{id:req.params.id}})
+        res.status(200).send("DONE");
+    }catch (error){
+        res.status(500).send("Internal Server Error");
+    }
+
 }
 // GET DOGS
 export async function getDogs(req: Request, res: Response) {
