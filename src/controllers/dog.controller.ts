8bbaec2898d49e0f1e2b98cb531f5f05c9702346
Kevin <kevin.pasteur@cpnv.ch>
2020-12-03 14:44:11 +0100

    add dog controller

--- src/controllers/dog.controller.ts
+++ src/controllers/dog.controller.ts
@@ -1,48 +1,50 @@
-// import {Request, Response} from "express";
-// import {Dog} from "../models/dog.model";
+import {Request, Response} from "express";
+import { Breed } from "../models/breed.model";
+import { Client } from "../models/client.model";
+import {Dog} from "../models/dog.model";
 
-// // POST
-// export async function post(req: Request, res: Response) {
-//     try{
-//         const dog = await Dog.create(req.body)
-//         res.status(201).send("DONE");
-//     } catch (error){
-//         res.status(500).send("Internal Server Error")
-//     }
-// }
-// // GET BY ID
-// export async function getById(req: Request, res: Response) {
-//     try{
-//         const dog = await Dog.findByPk(req.params.id)
-//         res.status(200).send(dog);
-//     } catch (error){
-//         res.status(404).send("Client not found")
-//     }
-// }
-// // GET
-// export async function get(req: Request, res: Response) {
-//     try{
-//         const dogs = await Dog.findAll()
-//         res.status(200).send(dogs);
-//     } catch (error){
-//         res.status(404).send("Client not found")
-//     }
-// }
-// // PUT
-// export async function put(req: Request, res: Response) {
-//     try{
-//         const dog = await Dog.update(req.body,{where:{id:req.params.id}})
-//         res.status(200).send("DONE");
-//     } catch (error){
-//         res.status(500).send("Internal Server Error");
-//     }
-// }
-// // REMOVE
-// export async function remove(req: Request, res: Response) {
-//     try{
-//         const dog = await Dog.destroy({where:{id:req.params.id}})
-//         res.status(200).send("DONE");
-//     }catch (error){
-//         res.status(500).send("Internal Server Error");
-//     }
-// }
+// POST
+export async function post(req: Request, res: Response) {
+    try{
+        const dog = await Dog.create(req.body)
+        res.status(201).send("DONE");
+    } catch (error){
+        res.status(500).send("Internal Server Error")
+    }
+}
+// GET BY ID
+export async function getById(req: Request, res: Response) {
+    try{
+        const dog = await Dog.findByPk(req.params.id,{include:[Client]})
+        res.status(200).send(dog);
+    } catch (error){
+        res.status(404).send("Client not found")
+    }
+}
+// GET
+export async function get(req: Request, res: Response) {
+    try{
+        const dogs = await Dog.findAll({include:[Client,Breed]})
+        res.status(200).send(dogs);
+    } catch (error){
+        res.status(404).send("Client not found")
+    }
+}
+// PUT
+export async function put(req: Request, res: Response) {
+    try{
+        const dog = await Dog.update(req.body,{where:{id:req.params.id}})
+        res.status(200).send("DONE");
+    } catch (error){
+        res.status(500).send("Internal Server Error");
+    }
+}
+// REMOVE
+export async function remove(req: Request, res: Response) {
+    try{
+        const dog = await Dog.destroy({where:{id:req.params.id}})
+        res.status(200).send("DONE");
+    }catch (error){
+        res.status(500).send("Internal Server Error");
+    }
+}
