cb787054760ad23eb8e787cdc7317faa89ce8da3
Kevin <kevin.pasteur@cpnv.ch>
2020-12-09 11:18:02 +0100

    Remove //

--- src/controllers/consultation.controller.ts
+++ src/controllers/consultation.controller.ts
@@ -1,48 +1,48 @@
-// import {Request, Response} from "express";
-// import {Consultation} from "../models/consultation.model";
+import {Request, Response} from "express";
+import {Consultation} from "../models/consultation.model";
 
-// // POST
-// export async function post(req: Request, res: Response) {
-//     try{
-//         const consultation = await Consultation.create(req.body)
-//         res.status(201).send("DONE");
-//     } catch (error){
-//         res.status(500).send("Internal Server Error")
-//     }
-// }
-// // GET BY ID
-// export async function get(req: Request, res: Response) {
-//     try{
-//         const consultation = await Consultation.findByPk(req.params.id);
-//         res.status(200).send(consultation);
-//     } catch (error){
-//         res.status(404).send("Consultation not found")
-//     }
-// }
-// // GET
-// export async function getAll(req: Request, res: Response) {
-//     try{
-//         const consultations = await Consultation.findAll();
-//         res.status(200).send(consultations);
-//     } catch (error){
-//         res.status(404).send("Consultations not found")
-//     }
-// }
-// // PUT
-// export async function put(req: Request, res: Response) {
-//     try{
-//         const consultation = await Consultation.update(req.body,{where:{id:req.params.id}})
-//         res.status(200).send("DONE");
-//     } catch (error){
-//         res.status(500).send("Internal Server Error");
-//     }
-// }
-// // REMOVE
-// export async function remove(req: Request, res: Response) {
-//     try{
-//         const consultation = await Consultation.destroy({where:{id:req.params.id}})
-//         res.status(200).send("DONE");
-//     }catch (error){
-//         res.status(500).send("Internal Server Error");
-//     }
-// }
+// POST
+export async function post(req: Request, res: Response) {
+    try{
+        const consultation = await Consultation.create(req.body)
+        res.status(201).send("DONE");
+    } catch (error){
+        res.status(500).send("Internal Server Error")
+    }
+}
+// GET BY ID
+export async function get(req: Request, res: Response) {
+    try{
+        const consultation = await Consultation.findByPk(req.params.id);
+        res.status(200).send(consultation);
+    } catch (error){
+        res.status(404).send("Consultation not found")
+    }
+}
+// GET
+export async function getAll(req: Request, res: Response) {
+    try{
+        const consultations = await Consultation.findAll();
+        res.status(200).send(consultations);
+    } catch (error){
+        res.status(404).send("Consultations not found")
+    }
+}
+// PUT
+export async function put(req: Request, res: Response) {
+    try{
+        const consultation = await Consultation.update(req.body,{where:{id:req.params.id}})
+        res.status(200).send("DONE");
+    } catch (error){
+        res.status(500).send("Internal Server Error");
+    }
+}
+// REMOVE
+export async function remove(req: Request, res: Response) {
+    try{
+        const consultation = await Consultation.destroy({where:{id:req.params.id}})
+        res.status(200).send("DONE");
+    }catch (error){
+        res.status(500).send("Internal Server Error");
+    }
+}
