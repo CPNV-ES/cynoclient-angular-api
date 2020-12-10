9b44d1c22372cc07715bda8fdefc1693743cacb6
Kevin <kevin.pasteur@cpnv.ch>
2020-12-10 13:43:23 +0100

    add include Service

--- src/controllers/consultation.controller.ts
+++ src/controllers/consultation.controller.ts
@@ -1,5 +1,7 @@
 import {Request, Response} from "express";
 import {Consultation} from "../models/consultation.model";
+import { Locality } from "../models/locality.model";
+import { Service } from "../models/service.model";
 
 // POST
 export async function post(req: Request, res: Response) {
@@ -13,7 +15,7 @@ export async function post(req: Request, res: Response) {
 // GET BY ID
 export async function get(req: Request, res: Response) {
     try{
-        const consultation = await Consultation.findByPk(req.params.id);
+        const consultation = await Consultation.findByPk(req.params.id,{include:[Service]});
         res.status(200).send(consultation);
     } catch (error){
         res.status(404).send("Consultation not found")
@@ -22,7 +24,7 @@ export async function get(req: Request, res: Response) {
 // GET
 export async function getAll(req: Request, res: Response) {
     try{
-        const consultations = await Consultation.findAll();
+        const consultations = await Consultation.findAll({include:[Service]});
         res.status(200).send(consultations);
     } catch (error){
         res.status(404).send("Consultations not found")
